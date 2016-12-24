f8aa9c5b662269175c50c530e5614035ab3331ee
angular-cli <angular-cli@angular.io>
2016-12-24 14:41:06 +0200

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EmployeeManagementAppPage } from './app.po';
+
+describe('employee-management-app App', function() {
+  let page: EmployeeManagementAppPage;
+
+  beforeEach(() => {
+    page = new EmployeeManagementAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
