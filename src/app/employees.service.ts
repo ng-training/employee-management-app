10f719e20e6490cf19aba971e0c946010b2bd330
Lucian Pacurar <luci.pacurar@gmail.com>
2017-03-15 21:42:22 +0200

    Move employees to private field.

--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -3,44 +3,47 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class EmployeesService {
 
+  private _employees = [{
+    name: 'John Doe',
+    position: 'software developer',
+    skills: ['javascript', 'html', 'css'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/83.jpg'
+  }, {
+    name: 'Johanan Stevan',
+    position: 'software developer',
+    skills: ['C#', '.net'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/33.jpg'
+  }, {
+    name: 'Archil Ezio',
+    position: 'team manager',
+    skills: ['planning', 'communication'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/43.jpg'
+  }, {
+    name: 'Kadek Matthew',
+    position: 'quality assurance',
+    skills: ['test plans', 'automated tests'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/13.jpg'
+  }, {
+    name: 'Rajesh Vincenzo',
+    position: 'quality assurance',
+    skills: ['requirements gathering', 'sql'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/14.jpg'
+  }, {
+    name: 'Aiolos Arash',
+    position: 'software developer',
+    skills: ['java', 'web'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/15.jpg'
+  }, {
+    name: 'Pontus Zdislav',
+    position: 'software developer',
+    skills: ['java', 'cloud'],
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/16.jpg'
+  }];
+
   constructor() { }
 
   getEmployees() {
-    return [{
-      name: 'John Doe',
-      position: 'software developer',
-      skills: ['javascript', 'html', 'css'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/83.jpg'
-    }, {
-      name: 'Johanan Stevan',
-      position: 'software developer',
-      skills: ['C#', '.net'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/33.jpg'
-    }, {
-      name: 'Archil Ezio',
-      position: 'team manager',
-      skills: ['planning', 'communication'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/43.jpg'
-    }, {
-      name: 'Kadek Matthew',
-      position: 'quality assurance',
-      skills: ['test plans', 'automated tests'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/13.jpg'
-    }, {
-      name: 'Rajesh Vincenzo',
-      position: 'quality assurance',
-      skills: ['requirements gathering', 'sql'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/14.jpg'
-    }, {
-      name: 'Aiolos Arash',
-      position: 'software developer',
-      skills: ['java', 'web'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/15.jpg'
-    }, {
-      name: 'Pontus Zdislav',
-      position: 'software developer',
-      skills: ['java', 'cloud'],
-      avatar: 'https://randomuser.me/api/portraits/thumb/men/16.jpg'
-    }];
+    
+    return this._employees;
   }
 }
