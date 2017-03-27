0eec62df646dd2d942f2010118c7b89d3bff3577
Adrian Fâciu <adrian.faciu@gmail.com>
2017-03-27 21:09:46 +0300

    Keep small templates inline. Removed empty style files.

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
 
