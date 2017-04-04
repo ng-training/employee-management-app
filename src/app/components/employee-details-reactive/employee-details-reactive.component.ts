8edb8735ae2512d073b854c6387691634b869430
Adrian Fâciu <adrian.faciu@gmail.com>
2017-04-04 22:52:04 +0300

    Reading values from employee model

--- src/app/components/employee-details-reactive/employee-details-reactive.component.ts
+++ src/app/components/employee-details-reactive/employee-details-reactive.component.ts
@@ -1,30 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { EmployeesService } from '../../core/index';
 
 @Component({
   selector: 'app-employee-details-reactive',
   templateUrl: './employee-details-reactive.component.html',
 })
-export class EmployeeDetailsReactiveComponent {
-  employeeName = new FormControl('', [Validators.required, Validators.minLength(5)]);
-  employeePosition = new FormControl('', [Validators.required, this.hasDigit(2, 'MustHave2')]);
-  employeeSkills = new FormControl('');
+export class EmployeeDetailsReactiveComponent implements OnInit {
+  employee: any;
 
-  employeeDetailsForm: FormGroup = this.formBuilder.group({
-    employeeName: this.employeeName,
-    employeePosition: this.employeePosition,
-    employeeSkills: this.employeeSkills
-  });
+  employeeName: FormControl;
+  employeePosition: FormControl;
+  employeeDetailsForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private employeesService: EmployeesService,
+  ) { }
 
-   onSubmit() {
-     console.log(this.employeeDetailsForm.value);
-   }
+  onSubmit() {
+    console.log(this.employeeDetailsForm.value);
+  }
 
-   hasDigit(digit: number, error: string) {
-     return function(input: FormControl) {
-       return input.value.indexOf(digit) >= 0 ? null : { [error]: true };
-     };
-   }
+  hasDigit(digit: number, error: string) {
+    return function (input: FormControl) {
+      return input.value.indexOf(digit) >= 0 ? null : { [ error ]: true };
+    };
+  }
+
+  ngOnInit() {
+    const name = this.route.snapshot.params[ 'id' ];
+    this.employee = this.employeesService.getEmployeeByName(name);
+
+    this.setupForm();
+  }
+
+  private setupForm() {
+    this.employeeName = new FormControl(this.employee.name, [ Validators.required, Validators.minLength(5) ]);
+    this.employeePosition = new FormControl(this.employee.position, [ Validators.required, this.hasDigit(2, 'MustHave2') ]);
+
+    this.employeeDetailsForm = this.formBuilder.group({
+      employeeName: this.employeeName,
+      employeePosition: this.employeePosition,
+    });
+  }
 }
