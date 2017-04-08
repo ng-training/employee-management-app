1cd29c39205615aa8af30709137cd8cdb8c46dc7
Adrian Fâciu <adrian.faciu@gmail.com>
2017-04-08 09:42:12 +0300

    Adding a new filter stream

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../core/index';
 
 import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/startWith';
 
 @Component({
   selector: 'app-employee-list',
@@ -19,6 +22,7 @@ export class EmployeeListComponent {
   employees: Array<any>;
 
   searchStream$ = new Subject<string>();
+  filterStream$ = new BehaviorSubject<boolean>(false);
 
   constructor(private employeeService: EmployeeService) {
     this._employees = employeeService.getEmployees();
@@ -27,11 +31,18 @@ export class EmployeeListComponent {
     this.searchStream$.debounceTime(200)
                       .distinctUntilChanged()
                       .filter(s => s.length > 2)
-                      .subscribe(searchTerm => this.filterEmployees(searchTerm));
+                      .withLatestFrom(this.filterStream$)
+                      .subscribe(([ searchTerm, onlyDevs ]) => this.filterEmployees(searchTerm, onlyDevs));
   }
 
-  filterEmployees(text: string) {
+  filterEmployees(text: string, onlyDevs: boolean) {
     const hasSearchText = text && text.length > 0;
-    this.employees = hasSearchText ? this._employees.filter(e => e.name.toLocaleLowerCase().includes(text.toLocaleLowerCase())) : this._employees;
+    this.employees = hasSearchText ?
+      this._employees.filter(empFilterFunction, { text, onlyDevs }) : this._employees;
   }
+}
+
+function empFilterFunction(employee: any) {
+  console.log(this.onlyDevs);
+  return employee.name.toLocaleLowerCase().includes(this.text.toLocaleLowerCase());
 }
