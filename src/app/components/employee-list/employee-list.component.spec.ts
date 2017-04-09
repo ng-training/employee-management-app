7d1e903009744bbef765deace54c6ec3cca349ac
Lucian Pacurar <luci.pacurar@gmail.com>
2017-04-09 18:40:52 +0300

    Add component tests

--- src/app/components/employee-list/employee-list.component.spec.ts
+++ src/app/components/employee-list/employee-list.component.spec.ts
@@ -1,16 +1,18 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { EmployeeListComponent } from './employee-list.component';
-import { EmployeeService } from '../../core/services/employee/employee.service';
+import { EmployeeService, employees } from '../../core/services/employee/employee.service';
 import { LoggerService } from '../../core/services/logger/logger.service';
+import { Observable } from "rxjs";
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
+  let employeeService: EmployeeService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,10 +27,82 @@ describe('EmployeeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    employeeService = fixture.debugElement.injector.get(EmployeeService);
+    spyOn(employeeService, 'getEmployees').and.returnValue(Observable.from([employees]));
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load the employees list',
+    async(() => {
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(component.employees instanceof Array).toBeTruthy();
+        expect(component.employees.length).toBe(10);
+      });
+    })
+  );
+
+  it('should load the employees list (fakeAsync)',
+    fakeAsync(() => {
+      fixture.detectChanges();
+      tick();
+      expect(component.employees instanceof Array).toBeTruthy();
+      expect(component.employees.length).toBe(10);
+    })
+  );
+});
+
+describe('EmployeeListComponent with stub', () => {
+  let component: EmployeeListComponent;
+  let fixture: ComponentFixture<EmployeeListComponent>;
+  const employeeServiceStub = {
+    getEmployees() {
+      return Observable.from([employees]);
+    }
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmployeeListComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{
+        provide: EmployeeService,
+        useValue: employeeServiceStub
+      }]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should load the employees list',
+    async(() => {
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        let firstItem = fixture.debugElement.query(By.css(".employee-list .list-group-item:first-child h4"));
+        expect(component.employees instanceof Array).toBeTruthy();
+        expect(component.employees.length).toBe(10);
+        expect(firstItem.nativeElement.textContent).toBe("Loraine Bradford");
+      });
+    })
+  );
+
+  it('should load the employees list (fakeAsync)',
+    fakeAsync(() => {
+      fixture.detectChanges();
+      tick();
+      expect(component.employees instanceof Array).toBeTruthy();
+      expect(component.employees.length).toBe(10);
+    })
+  );
 });
