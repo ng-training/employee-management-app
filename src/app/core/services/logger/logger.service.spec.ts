6ef9182504bfc24e8b7a43c6f1345d04d4836af8
Adrian Fâciu <adrian.faciu@gmail.com>
2017-04-09 12:16:57 +0300

    Adding logger tests

--- src/app/core/services/logger/logger.service.spec.ts
+++ src/app/core/services/logger/logger.service.spec.ts
@@ -3,13 +3,49 @@ import { TestBed, inject } from '@angular/core/testing';
 import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
+
+  let logger: LoggerService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [LoggerService]
-    });
+    logger = new LoggerService();
   });
 
-  it('should ...', inject([LoggerService], (service: LoggerService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('can create it', () => {
+    expect(logger).toBeTruthy();
+  });
+
+  it('can call log', () => {
+    spyOn(logger, 'log');
+
+    logger.log('foo');
+
+    expect(logger.log).toHaveBeenCalled();
+  });
+
+  // Who knows why we don't have coverage on the error method ?
+  it('can call error', () => {
+    spyOn(logger, 'error');
+
+    const errorMessage = 'Nasty error';
+    logger.error(errorMessage);
+
+    expect(logger.error).toHaveBeenCalledWith(errorMessage);
+  });
+
+  it('can call info implementation', () => {
+    spyOn(logger, 'info').and.callThrough();
+
+    logger.info('new message');
+
+    expect(logger.info).toHaveBeenCalled();
+  });
+
+  it('can call warning fake', () => {
+    spyOn(logger, 'warn').and.callFake(() => console.log('this is not happening'));
+
+    logger.warn('foo');
+
+    expect(logger.warn).toHaveBeenCalledTimes(1);
+  });
+
 });
