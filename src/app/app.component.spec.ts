1a7149de814bd6d53903c8ca0e02251c1fb0e23f
Adrian Fâciu <adrian.faciu@gmail.com>
2017-04-30 11:36:46 +0300

    Minor refactoring and fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from 'app/components';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        EmployeeListComponent,
       ],
     }).compileComponents();
   }));
@@ -23,10 +25,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app works!');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render visma logo', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('img').alt).toContain('Visma');
   }));
 });
