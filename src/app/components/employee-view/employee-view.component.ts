406f79459fcdc70cc460b95e16e49e68eed2f6cb
Adrian Fâciu <adrian.faciu@gmail.com>
2017-05-13 16:49:27 +0300

    Fixed default user image

--- src/app/components/employee-view/employee-view.component.ts
+++ src/app/components/employee-view/employee-view.component.ts
@@ -6,7 +6,8 @@ import { Component, Input } from '@angular/core';
    <img class="pull-left"
         *ngIf="employee"
         [src]="employee.picture"
-        [alt]="employee.name">
+        [alt]="employee.name"
+        width="48" height="48">
     <h4 class="list-group-item-heading">{{ employee?.name }}</h4>
     <p class="list-group-item-text">({{ employee?.position }})</p>
   `
