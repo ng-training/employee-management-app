cd01cfb073e3e82d206db69a7f0024401e59a431
Adrian Fâciu <adrian.faciu@gmail.com>
2017-05-15 14:13:03 +0300

    Removed unused style file

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
 
