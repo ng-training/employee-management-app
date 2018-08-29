473fd95ced506147de530aa3f5f0bff80f50b670
Lucian Pacurar <luci.pacurar@gmail.com>
2018-08-29 22:34:18 +0300

    Fix linting

--- src/app/core/pipes/pad-left/pad-left.pipe.ts
+++ src/app/core/pipes/pad-left/pad-left.pipe.ts
@@ -1,7 +1,7 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: "padLeft"
+  name: 'padLeft'
 })
 export class PadLeftPipe implements PipeTransform {
   transform(value: string, targetLength: number, padString: string): string {
